ecc3814ee9f6d67c30ba9427b16150615338611c
jessebeach50 <75637242+jessebeach50@users.noreply.github.com>
2024-09-18 16:47:45 -0400

    Update HouseModel.js

--- HouseModel.js
+++ HouseModel.js
@@ -1,4 +1,4 @@
-var pollingAverage = 2.02;
+var pollingAverage = 2.52;
 var pollingErrorInMonth = 0;
 var pollingError = 4;
 var maxDemocraticResult = pollingAverage + pollingErrorInMonth + pollingError;
